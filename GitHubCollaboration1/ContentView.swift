4eadaa1bfef87697ea0328adbeb0bbf018e87600
Valerie Hammer <vkellogg@wisc.edu>
2025-06-09 09:22:01 -0500

    Initial Commit

---
+++ GitHubCollaboration1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubCollaboration1
+//
+//  Created by Valerie Hammer on 6/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubCollaboration1/GitHubCollaboration1App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaboration1App.swift
+//  GitHubCollaboration1
+//
+//  Created by Valerie Hammer on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCollaboration1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
